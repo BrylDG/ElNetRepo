44d378d9b4149499f32d77d134fed793761b3bb7
AmaroKobe03 <148180353+AmaroKobe03@users.noreply.github.com>
2025-02-09 22:59:27 +0800

    Landing page

---
+++ Subdivision-Monitoring-System/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace Subdivision_Monitoring_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+}
